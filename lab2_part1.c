9a81d9f5c4f72ba84e6a96b3a22b8e008a9e21c8
cchoi27 <christine.choi344@gmail.com>
2024-01-29 01:54:41 -0800

    done part1

--- lab2_part1.c
+++ lab2_part1.c
@@ -8,7 +8,7 @@ Write a program that:
 - gradually slows the motors to a stop
 - does the same in the reverse motor direction and continuously repeats
 - while the program is running, print the motor speed on the screen
-
+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
 */
 #include "globals.h"
 #include <util/delay.h>
@@ -16,54 +16,79 @@ Write a program that:
 #include <avr/interrupt.h>
 #include <stdio.h>
 
-/**void motor(uint8_t num, int8_t speed)
-{
-        if (speed == 30) {
-                set_servo(num, 255);
-        }
-        else if (speed == -30) {
-                set_servo(num, 97);    
-        }
-        else if (speed == 0) {
-                set_servo(num, 127);
-        }
-}
-*/
 
 void motor(uint8_t num, int8_t speed)
-{       
-        int16_t adjusted_speed;
-
-
-        if (num) //wheel 1
+{ //num will be 0 or 1 corresponding to the left or right motor
+  // speed will be a number from -100 (full speed reverse) to 
+  //+100 (full speed forward).
+        uint8_t adjusted_speed;
+        if (num == 0) //wheel 0
         {
                 adjusted_speed = (speed*3)/10 + 127;
-                lcd_cursor(0,1);
-                print_string("1: ");
-                print_num(adjusted_speed);
-                set_servo(1, adjusted_speed); //fast
-        }
-        else    //wheel 0
-        {
-                adjusted_speed = (-speed*3)/10 + 127;
-                lcd_cursor(0, 0);
+                lcd_cursor(0,0);
                 print_string("0: ");
                 print_num(adjusted_speed);
                 set_servo(0, adjusted_speed); //fast
         }
+        else    //wheel 1
+        {
+                adjusted_speed = (-speed*3)/10 + 127;
+                lcd_cursor(0, 1);
+                print_string("1: ");
+                print_num(adjusted_speed);
+                set_servo(1, adjusted_speed); //fast
+        }
 }
 
 
 
+void full_speed(uint8_t direction)
+{//gradually spins the motors to full speed forward (1)
+//or backwards (0)
+int8_t max = (direction)?(100):(-100);
+
+int8_t i = 0;
+while (i != max)
+{ 
+        motor(0, i);
+        motor(1, i);
+        _delay_ms(100);
+        if (direction)
+                i++;
+        else
+                i--;
+}
+}
+
+void gradually_stop(uint8_t direction)
+{//gradually slows the motors to a stop
+//forward (1) or backwards (0)
+int8_t i = (direction)?(100):(-100);
+while (i != 0)
+{ 
+        motor(0, i);
+        motor(1, i);
+        _delay_ms(100);
+        if (direction)
+                i--;
+        else
+                i++;
+}
+}
 
 int main(void) {
    init();  //initialize board hardware
+   motor(0, 100);
+   motor(1, 100);
 
 
 while(1) 
 {
-        motor(1, 100);
-        motor(0, 100);
+        full_speed(1);
+        gradually_stop(1);
+
+        full_speed(0);
+        gradually_stop(0);
 
 
 }
