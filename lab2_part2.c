2ae56995ea9ca6b4cbb1e5d337b03f9eddf96dd9
Angelika Canete <acanete@calpoly.edu>
2024-01-29 21:35:32 -0800

    complete part 2

--- lab2_part2.c
+++ lab2_part2.c
@@ -14,7 +14,25 @@ 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
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <stdio.h>
+#include <stdbool.h>
 
+u16 button_delay_check(u16 loop)
+{
+    //acts as a delay_ms function that also checks for a button press
+
+        u16 count = 0;
+        int button_flag = 0;
+        while (count < loop)
+        {
+            if (get_btn()==1)
+            {
+                button_flag = 1;
+            }
+            _delay_ms(1);
+            count ++;
+        }
+        return button_flag;
+}
 
 void motor(uint8_t num, int8_t speed)
 { //num will be 0 or 1 corresponding to the left or right motor
@@ -50,38 +68,92 @@ void sensor_update()
 }
 void fear(int state1, int state2) {
 	if (analog(3) > state1) {
-		motor(1, (state1 + state1 * .10));
+		motor(1, (state1 - (state1 * .50)));
 		state1 = analog(3);
 	//	_delay_ms(500);
 	}
 	else {
-		motor(1, (state1 - state1 * .10));
+		motor(1, (state1 + (state1 * .50)));
 		state1 = analog(3);
 	}
 	if (analog (4) > state2) {
-		motor(0, (state2 + state2*.10));
+		motor(0, (state2 - (state2*.50)));
 		state2 = analog(4);
 	//	_delay_ms(500);
 	}
 	else {
 
-		motor(0, (state2 - state2*.10));
+		motor(0, (state2 + (state2*.50)));
 		state2 = analog(4);
 	}
 
 }
+
+
+void aggression(int state1, int state2) {
+	if (analog(3) > state1) {
+		motor(1, (state1 + (state1 * .50)));
+		state1 = analog(3);
+	//	_delay_ms(500);
+	}
+	else {
+		motor(1, (state1 - (state1 * .50)));
+		state1 = analog(3);
+	}
+	if (analog (4) > state2) {
+		motor(0, (state2 + (state2*.50)));
+		state2 = analog(4);
+	//	_delay_ms(500);
+	}
+	else {
+
+		motor(0, (state2 - (state2*.50)));
+		state2 = analog(4);
+	}
+
+}
+
 
 int main(void) {
    init();  //initialize board hardware
    motor(0, 0);
    motor(1, 0);
 
-int state1 = 50;
-int state2 = 50;
+int fstate1 = 50;
+int fstate2 = 50;
+
+int astate1 = 50;
+int astate2 = 50;
+
+bool flag = false;
+
 while(1) 
 {  
-        sensor_update();
-	fear(state1, state2);
+	while (flag == false) {
+		_delay_ms(300);
+		clear_screen();
+		print_string("Fear");
+		fear(fstate1, fstate2);
+
+		if (button_delay_check(1) == 1) {
+			flag = true;
+			break;
+		}
+	}
+	while (flag) {
+		_delay_ms(300);
+		clear_screen();
+		print_string("Aggression");
+		aggression(astate1, astate2);
+
+		if (button_delay_check(1) == 1) {
+			flag = false;
+			break;
+		}
+	}
+
+//	motor(0,0);
+//	motor(1,0);
 
         
 
