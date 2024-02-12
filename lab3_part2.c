537faf8244d574d56ddebccdb696ab5585aa8575
cchoi27 <christine.choi344@gmail.com>
2024-02-12 11:15:09 -0800

    accel thing

--- lab3_part2.c
+++ lab3_part2.c
@@ -233,8 +233,60 @@ If a sensor does not see the corrrect value,  correct by the PDI
 }
 
 
+void delay(u16 loop)
+{
+        u16 count = 0;
+        while (count < loop)
+        {
+                _delay_ms(1);
+                count ++;
+        }
+}
 
-int main(void) {
+
+
+void display_tiltable_text()
+{
+    //start position
+    print_num(416);
+
+    int row = 0;
+    int col = 4;
+
+    while(1)
+    {
+        if ((get_accel_y() > 30 && get_accel_y()< 70 ) & (col > 0) )
+        {//to the left
+            col--;
+        }
+        if ( (get_accel_y() > 200 && get_accel_y()  < 250 ) & (col < 5) ) 
+        {//to the right
+            col++;
+        }
+
+        if ((get_accel_x() > 200 && get_accel_x()< 230 ))
+        {// move down
+            row = 1;
+        }
+        if ( (get_accel_x() > 20 && get_accel_x() < 70 ) ) 
+        {//move up
+            row = 0;
+        }
+
+        //print 416
+        clear_screen();
+        lcd_cursor(col, row);
+        print_num(416);
+        delay(50);
+
+    }
+
+}
+
+
+
+int main(void) 
+{
    init();  //initialize board hardware
    motor(0, 0);
    motor(1, 0);
@@ -243,10 +295,18 @@ int main(void) {
    line_seeking_PID();
 
 
+//data collection
    motor(0, 0);
    motor(1, 0);
    data_collection();
 
+//get training interations
+
+
+//training mode
+
+
+//nerual network
 
 return 0;
 }
